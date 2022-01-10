fe2ef42128c57a6c211593f9ce52318b2ea8ff12
Phurba Lama <phurbalama2002@gmail.com>
2022-01-10 18:16:44 -0500

    first commit

---
+++ src/main/java/com/phurbalama/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,50 @@
+package com.phurbalama.webservices.restfulwebservices.helloworld;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller means handling http request
+@RestController
+public class HelloWorldController {
+	@Autowired
+	private MessageSource messageSource;
+	//GET
+	//URI - /hello-world
+	//method - "Hello World"
+	//@RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+	// GET
+	// URI - /hello-world-bean
+	// method - "Hello World from Bean class"
+	// @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	//Get using path Variable
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path="/hello-world-internationalize")
+	public String helloWorldInternationalized(
+			//@RequestHeader(name="Accept-language", required =false) Locale locale
+			) {
+		return messageSource.getMessage("good.morning.message",null,"Default Message",LocaleContextHolder.getLocale());
+	}
+	
+
+}
